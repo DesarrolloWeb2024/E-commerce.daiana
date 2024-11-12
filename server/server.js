335c32b4eeb69d50db61a0837223478bd762e4fd
daiana1996 <daianatjm@gmail.com>
2024-11-12 12:45:27 -0300

    de la lista de productos , cambié en la carpeta JS server.js las imágenes y las descripciones

--- server/server.js
+++ server/server.js
@@ -12,31 +12,31 @@ app.get('/api/products', (req, res) => {
   const products = [
     { 
       id: 1, 
-      name: 'Cámara web HD 1080P', 
-      oldPrice: 37500, 
-      price: 35000, 
-      urlImg: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSmYckEMeFKNyPfp2TJu1Fup4HIBjibtiCqO3I6nbhWev7LIyxevl52_-aZwkaoJmuOVxzl0GTcx9nqq5G4T3ArSy99H_T5&usqp=CAE'
+      name: 'Jean', 
+      oldPrice: 40000, 
+      price: 37000, 
+      urlImg: 'https://i.pinimg.com/736x/5c/c2/19/5cc219fa3354e2534a6cf3ea226e0eb4.jpg'
     },
     {
       id: 2, 
-      name: 'Campera Keyboard programador',  
-      oldPrice: 11500, 
-      price: 11200, 
-      urlImg: 'https://purodiseno.lat/wp-content/uploads/2023/04/CAMPERA-TECLADO-2-821x1024.jpg' 
+      name: 'Vestido floreado',  
+      oldPrice: 35000, 
+      price: 30000, 
+      urlImg: 'https://i.pinimg.com/originals/cc/83/c6/cc83c6d1e7c62ac2b52df6dfbf17b526.jpg' 
     },
     { 
       id: 3, 
-      name: 'Disco-solido interno kingston sata-960g',  
-      oldPrice: 18500, 
-      price: 18000, 
-      urlImg: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQwqHX1fVP2DUn6mEw05ukydZ6KOgkf0w3N3ZEHHJnDUW7WtxSEzdOZ73YxCn-FBC5kYxGeF4wy9zakpXVDOXbjQouNAmyH1LTbzkyuK2t0u9We1eAqfkFFDw&usqp=CAE' 
+      name: 'Conjunto',  
+      oldPrice: 10000, 
+      price: 9000, 
+      urlImg: 'https://th.bing.com/th/id/R.caac9ddfd5c6a84bde31e93e7c0dcb5e?rik=q9mXfxp5fSheLw&pid=ImgRaw&r=0' 
     },
     { 
       id: 3, 
-      name: 'Taza Código De Programacion',  
+      name: 'Blusa',  
       oldPrice: 3500, 
       price: 2200, 
-      urlImg: 'https://http2.mlstatic.com/D_NQ_NP_786656-MLA46832914211_072021-O.webp' 
+      urlImg: 'https://th.bing.com/th/id/OIP.EUsPsum5IyI5DcWXRgrxgAAAAA?rs=1&pid=ImgDetMain' 
     },
     
     
@@ -48,3 +48,4 @@ app.get('/api/products', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+//ar.pinterest.com/pin/1055599905474809/
